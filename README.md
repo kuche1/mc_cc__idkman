72c396387b9ecb65ad87d7d9758559b0ea739983
me <a@a.a>
2021-09-18 02:01:24 +0300

    asd

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mc_cc__idkman
+
+wget https://raw.githubusercontent.com/kuche1/mc_cc__idkman/master/update.lua
+./update.lua
--- update.lua
+++ update.lua
@@ -1,17 +1,19 @@
 
 
-local fname = "./sus.lua"
+function update_a_file(fname)
 
-fs.delete(fname)
+	fs.delete(fname)
 
-local req = http.get("https://raw.githubusercontent.com/kuche1/mc_cc__idkman/master/sus.lua")
+	local req = http.get("https://raw.githubusercontent.com/kuche1/mc_cc__idkman/master/sus.lua")
 
-local data = req.readAll()
+	local data = req.readAll()
 
-local f = fs.open(fname, "w")
+	local f = fs.open(fname, "w")
 
-f.write(data)
+	f.write(data)
 
-f.close()
+	f.close()
 
+end
 
+update_a_file("./sus.lua")
