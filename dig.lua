8ff50ef7bf4d36438433773677e6c693cee9268a
me <a@a.a>
2021-09-18 20:41:13 +0300

    asd

--- dig.lua
+++ dig.lua
@@ -1,21 +1,98 @@
 
 ---- TODO
 -- resource recognition
+-- check if empty
 
 local FUEL_IND = 16
+local PICKUP_IND = 1
 
-function dig(leny, lenx)
+local BLACKLIST = {
+	"minecraft:dirt",
+	"minecraft:cobblestone",
+	}
+
+-- wrapper
+
+function dig_wrapper_pre()
+	if turtle.getItemCount() ~= 0 then
+		error("pickup field not empty")
+	end
+end
+
+function dig_wrapper_post()
+
+	local item = turtle.getItemDetail()
+	if item == nil then
+		return
+	end
+
+	local name = item.name
+	
+	for i=1,#BLACKLIST do
+		local item = BLACKLIST[i]
+		if item == name then
+			turtle.drop()
+			return
+		end
+	end
+
+	print("picked up "..name)
+
+	local available = 0
+	for i=1,16 do
+		if i ~= PICKUP_IND then
+			local item = turtle.getItemDetail(i)
+			if item == nil then
+				available = i
+			else
+				if item.name == name then
+					turtle.transferTo(i)
+					if turtle.getItemCount() == 0 then
+						return
+					end
+				end
+			end
+		end
+	end
+
+	if available ~= 0 then
+		turtle.transferTo(available)
+		return
+	end
+
+	error("not enough space")
+
+end
+
+
+function dig()
+	dig_wrapper_pre()
+	turtle.dig()
+	dig_wrapper_post()
+end
+
+function digUp()
+	dig_wrapper_pre()
+	turtle.digUp()
+	dig_wrapper_post()
+end
+
+--
+
+function dig_a_hole(leny, lenx)
 
 	-- fuel refill
 
 	turtle.select(FUEL_IND)
 	turtle.refuel()
 
+	turtle.select(PICKUP_IND)
+
 	while true do
 
 		-- init
 
-		turtle.dig()
+		dig()
 		turtle.forward()
 		turtle.turnRight()
 
@@ -28,14 +105,14 @@ function dig(leny, lenx)
 			local x = 1
 			while x < lenx do
 
-				turtle.dig()
+				dig()
 				turtle.forward()
 
 				x = x + 1
 			end
 
 			if y ~= leny then
-				turtle.digUp()
+				digUp()
 				turtle.up()
 				turtle.turnLeft()
 				turtle.turnLeft()
@@ -75,7 +152,7 @@ function dig_main(arg)
 	local leny = tonumber(arg[1])
 	local lenx = tonumber(arg[2])
 
-	return dig(leny, lenx)
+	return dig_a_hole(leny, lenx)
 	
 end
 
